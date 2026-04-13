f97a2f4b12e04e53703c037b3043335046ef30de
selinaurshibididididiid <zengselina760@gmail.com>
2026-04-13 23:18:32 +0000

    Finished the wireframe and added a description at the top of the html as well as finished the create flashcard section (user input and output) on both the html and the javascript

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// add javascript here
+//store and add new questions and answers 
+const questionInput = document.getElementById("questionInput").value;
+const answerInput = document.getElementById("answerInput").value;
+const addCardBtn = document.getElementById("addCardBtn");

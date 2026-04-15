2d4ec018586d1ecd844793ff0001b47c61f52488
selinaurshibididididiid <zengselina760@gmail.com>
2026-04-15 00:17:55 +0000

    finished the entire user input of question and answer and is able to display with flashcards, as well as make the basic shape of flashcards and made them so they can flip

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
 //store and add new questions and answers 
-const questionInput = document.getElementById("questionInput").value;
-const answerInput = document.getElementById("answerInput").value;
+const questionInput = document.getElementById("questionInput");
+const answerInput = document.getElementById("answerInput");
 const addCardBtn = document.getElementById("addCardBtn");
+
+//store user input into arrays 
+const questions = [];
+const answers = []; 
+
+//Store onto HTML as now its only stored on Java
+function displayFlashcards() {
+    const container = document.getElementById("flashcardContainer");
+    container.innerHTML = "";
+
+    //goes through flashcards one by one!!!
+    for (let i = 0; i < questions.length; i++) {
+        //creates new card element in Javascript for html as the input is unknown (user's input) and not fixed input (like so html)
+        const card = document.createElement("div");
+        //OUTER container
+        card.classList.add("card");
+
+        //final result: <div class="card"></div>
+
+        //html code (INNER CONTAINER) 
+        //get the item at position i
+        card.innerHTML = `
+            <div class="front">${questions[i]}</div>
+            <div class="back">${answers[i]}</div>
+        `;
+
+        // flip on click, CSS does not react to javascript code --> html code IN javascript
+        card.addEventListener("click", function () {
+            card.classList.toggle("flip");
+        });
+
+        //displayed on the website as it is plugged BACk into HTML
+        container.appendChild(card);
+    }
+}
+
+//start the entire function when the user clicks the button and changes the user input into a string
+addCardBtn.addEventListener("click", function() {
+    const question = questionInput.value
+    const answer = answerInput.value
+
+//prevent INCOMPLETE flashcards (if either container is empty, the function is immediately stopped and returned to the top)
+    if (question === "" || answer === "") return;
+
+//adds the new item into the end of the array
+    questions.push(question);
+    answers.push(answer);
+
+//for the visuals (CSS styling and more code TOP)
+    displayFlashcards();
+
+//after completing the code, the 2 inputs will be cleared for new input
+    questionInput.value = "";
+    answerInput.value = "";
+});
+
